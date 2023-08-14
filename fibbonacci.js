d8e233e3ef45ec2a2260045260a5d07f13f7f973
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-08-14 17:32:15 +0300

    Add comments

--- fibbonacci.js
+++ fibbonacci.js
@@ -7,10 +7,10 @@ const fibs = n => {
     return fibArray;
 }
 
-console.log(fibs(1));
-console.log(fibs(2));
-console.log(fibs(8));
-console.log(fibs(16));
+console.log(fibs(1)); // [0]
+console.log(fibs(2)); // [0, 1]
+console.log(fibs(8)); // [0, 1, 1,  2, 3, 5, 8, 13]
+console.log(fibs(16)); // [0, 1, 1, 2, 3, 5,  8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
 
 
 const fibsRec = (n) => {
@@ -19,7 +19,7 @@ const fibsRec = (n) => {
     return [...fibsRec(n-1), fibsRec(n-1)[n-2]+fibsRec(n-1)[n-3]];
 }
 
-console.log(fibsRec(1));
-console.log(fibsRec(2));
-console.log(fibsRec(8));
-console.log(fibsRec(16));
+console.log(fibsRec(1)); // [0]
+console.log(fibsRec(2)); // [0, 1]
+console.log(fibsRec(8)); // [0, 1, 1,  2, 3, 5, 8, 13]
+console.log(fibsRec(16)); // [0, 1, 1, 2, 3, 5,  8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
